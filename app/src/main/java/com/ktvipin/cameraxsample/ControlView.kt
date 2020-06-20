d699ed0616fb295d73a1f4436837556e51a53cbb
VipinKT <ktvipin27@gmail.com>
2020-06-20 10:27:02 +0530

    timer view created

--- app/src/main/java/com/ktvipin/cameraxsample/ControlView.kt
+++ app/src/main/java/com/ktvipin/cameraxsample/ControlView.kt
@@ -4,19 +4,20 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.os.Handler
-import android.os.SystemClock
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.Gravity
+import android.view.HapticFeedbackConstants
 import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import com.ktvipin.cameraxsample.AnimationUtils.startBlinkAnimation
 import com.ktvipin.cameraxsample.AnimationUtils.startRotateAnimation
 import com.ktvipin.cameraxsample.AnimationUtils.startScaleAnimation
-import java.util.concurrent.TimeUnit
+import com.ktvipin.cameraxsample.Config.LONG_PRESS_DELAY_MILLIS
+import com.ktvipin.cameraxsample.Config.SCALE_DOWN
+import com.ktvipin.cameraxsample.Config.SCALE_UP
 
 
 /**
@@ -27,9 +28,9 @@ class ControlView : LinearLayout {
     interface Listener {
         fun toggleCamera()
         fun toggleFlash(flashMode: FlashMode)
+        fun capturePhoto()
         fun startVideoCapturing()
         fun stopVideoCapturing()
-        fun capturePhoto()
     }
 
     enum class FlashMode {
@@ -42,41 +43,9 @@ class ControlView : LinearLayout {
     private var flashMode: FlashMode = FlashMode.FLASH_MODE_OFF
     private var listener: Listener? = null
 
-    private var startHTime = 0L
-    private var timerHandler = Handler()
-    private val timerThread = object : Runnable {
-        override fun run() {
-            tvTimer.text = calculateDuration(SystemClock.uptimeMillis() - startHTime)
-            timerHandler.postDelayed(this, 0)
-        }
-    }
-
-    private val layoutTimer = LinearLayout(context).apply {
-        layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
-            background = context.getDrawable(R.drawable.bg_rounded_corner_gray)
-            gravity = Gravity.CENTER
-            setPadding(5.px, 2.px, 5.px, 2.px)
-            visibility = View.INVISIBLE
-        }
+    private val timerView = TimerView(context).apply {
+        layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
     }.also { addView(it) }
-
-    private val ivRedDot = ImageView(context).apply {
-        layoutParams = LayoutParams(6.px, 6.px).apply {
-            setImageResource(R.drawable.ic_red_dot_6dp)
-            setMargins(leftMargin, topMargin, 5.px, bottomMargin)
-            gravity = Gravity.CENTER
-        }
-    }.also { layoutTimer.addView(it) }
-
-    private val tvTimer = TextView(context).apply {
-        layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
-            gravity = Gravity.CENTER
-            setTextColor(Color.WHITE)
-            setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f)
-            text = "00:00"
-            setPaddingRelative(8, 5, 8, 5)
-        }
-    }.also { layoutTimer.addView(it) }
 
     private val layoutControls = LinearLayout(context).apply {
         layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
@@ -88,7 +57,6 @@ class ControlView : LinearLayout {
     private val ivFlash = ImageView(context).apply {
         layoutParams = LayoutParams(48.px, 48.px).apply {
             setImageResource(R.drawable.ic_flash_off_white_20dp)
-            //setMargins(leftMargin, topMargin, 5.px, bottomMargin)
             gravity = Gravity.CENTER
         }
         setOnClickListener { toggleFlash() }
@@ -102,17 +70,17 @@ class ControlView : LinearLayout {
             gravity = Gravity.CENTER
         }
     }.also {
+        isHapticFeedbackEnabled = true
         setupCaptureButtonListener(it)
         layoutControls.addView(it)
     }
 
     private val ivSwitchCam = ImageView(context).apply {
         layoutParams = LayoutParams(48.px, 48.px).apply {
-            setImageResource(R.drawable.ic_camera_swap_fill_white_24dp)
-            //setMargins(leftMargin, topMargin, 5.px, bottomMargin)
+            setImageResource(R.drawable.ic_baseline_flip_camera_24dp)
             gravity = Gravity.CENTER
             setOnClickListener {
-                startRotateAnimation(it)
+                it.startRotateAnimation()
                 listener?.toggleCamera()
             }
             setPadding(12.px, 12.px, 12.px, 12.px)
@@ -166,34 +134,30 @@ class ControlView : LinearLayout {
         var initialTouchY = 0f
         val mHandler = Handler()
         val mLongPressed = Runnable {
-            startVideoCapturing()
+            onLongPressCaptureButton()
         }
         capture.setOnTouchListener { v, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     mHandler.postDelayed(
                         mLongPressed,
-                        DELAY_MILLIS
+                        LONG_PRESS_DELAY_MILLIS
                     )
                     capture.setImageResource(R.drawable.ic_circle_red_white_24dp)
-                    startScaleAnimation(
+                    v.startScaleAnimation(
                         SCALE_UP,
-                        SCALE_UP, v
+                        SCALE_UP
                     )
                     initialTouchX = event.rawX
                     initialTouchY = event.rawY
                     return@setOnTouchListener true
                 }
                 MotionEvent.ACTION_UP -> {
-                    stopTimer()
+                    timerView.stopTimer()
                     mHandler.removeCallbacks(mLongPressed)
-                    startScaleAnimation(
-                        SCALE_DOWN,
-                        SCALE_DOWN, v,
-                        onAnimationEnd = {
-                            capture.setImageResource(R.drawable.ic_circle_line_white_24dp)
-                        }
-                    )
+                    v.startScaleAnimation(SCALE_DOWN, SCALE_DOWN) {
+                        capture.setImageResource(R.drawable.ic_circle_line_white_24dp)
+                    }
                     val xDiff = initialTouchX - event.rawX
                     val yDiff = initialTouchY - event.rawY
                     if ((kotlin.math.abs(xDiff) < 5) && (kotlin.math.abs(yDiff) < 5)) {
@@ -217,32 +181,11 @@ class ControlView : LinearLayout {
         }
     }
 
-    private fun startVideoCapturing() {
-        ivCapture.performHapticFeedback(ivCapture.id)
+    private fun onLongPressCaptureButton() {
+        ivCapture.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS)
         isVideoCapturing = true
         listener?.startVideoCapturing()
-        startHTime = SystemClock.uptimeMillis()
-        timerHandler.postDelayed(timerThread, 0)
-        layoutTimer.visibility = View.VISIBLE
-        startBlinkAnimation(ivRedDot)
-    }
-
-    private fun stopTimer() {
-        timerHandler.removeCallbacks(timerThread)
-        ivRedDot.clearAnimation()
-        layoutTimer.visibility = View.INVISIBLE
-        tvTimer.text = "00:00"
-    }
-
-    private fun calculateDuration(timeInMilliseconds: Long): String {
-        return String.format("%02d", TimeUnit.MILLISECONDS.toMinutes(timeInMilliseconds)).plus(
-            ":" + String.format(
-                "%02d",
-                TimeUnit.MILLISECONDS.toSeconds(timeInMilliseconds) - TimeUnit.MINUTES.toSeconds(
-                    TimeUnit.MILLISECONDS.toMinutes(timeInMilliseconds)
-                )
-            )
-        )
+        timerView.startTimer()
     }
 
     fun setFlashViewVisibility(visibility: Boolean) {
@@ -255,12 +198,5 @@ class ControlView : LinearLayout {
 
     fun setListener(listener: Listener?) {
         this.listener = listener
-    }
-
-    companion object {
-        private const val DELAY_MILLIS = 1000L
-        private const val SCALE_UP = 1.5f
-        private const val SCALE_DOWN = 1.0f
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
     }
 }
---
+++ app/src/main/java/com/ktvipin/cameraxsample/TimerView.kt
@@ -0,0 +1,87 @@
+package com.ktvipin.cameraxsample
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Handler
+import android.os.SystemClock
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.ktvipin.cameraxsample.AnimationUtils.startBlinkAnimation
+import java.util.concurrent.TimeUnit
+
+
+/**
+ * Created by Vipin KT on 18/06/20
+ */
+class TimerView : LinearLayout {
+
+    private var startTime = 0L
+    private var timerHandler = Handler()
+    private val timerThread = object : Runnable {
+        override fun run() {
+            tvTimer.text = calculateDuration(SystemClock.uptimeMillis() - startTime)
+            timerHandler.postDelayed(this, 0)
+        }
+    }
+
+    private val ivRedDot = ImageView(context).apply {
+        layoutParams = LayoutParams(6.px, 6.px).apply {
+            setImageResource(R.drawable.ic_red_dot_6dp)
+            setMargins(leftMargin, topMargin, 5.px, bottomMargin)
+            gravity = Gravity.CENTER
+        }
+    }.also { addView(it) }
+
+    private val tvTimer = TextView(context).apply {
+        layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
+            gravity = Gravity.CENTER
+            setTextColor(Color.WHITE)
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f)
+            text = "00:00"
+            setPaddingRelative(8, 5, 8, 5)
+        }
+    }.also { addView(it) }
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        background = context.getDrawable(R.drawable.bg_rounded_corner_gray)
+        gravity = Gravity.CENTER
+        setPadding(5.px, 2.px, 5.px, 2.px)
+        visibility = View.INVISIBLE
+    }
+
+    fun startTimer() {
+        startTime = SystemClock.uptimeMillis()
+        timerHandler.postDelayed(timerThread, 0)
+        visibility = View.VISIBLE
+        ivRedDot.startBlinkAnimation()
+    }
+
+    fun stopTimer() {
+        timerHandler.removeCallbacks(timerThread)
+        ivRedDot.clearAnimation()
+        visibility = View.INVISIBLE
+        tvTimer.text = "00:00"
+    }
+
+    private fun calculateDuration(timeInMilliseconds: Long): String {
+        return String.format("%02d", TimeUnit.MILLISECONDS.toMinutes(timeInMilliseconds)).plus(
+            ":" + String.format(
+                "%02d",
+                TimeUnit.MILLISECONDS.toSeconds(timeInMilliseconds) - TimeUnit.MINUTES.toSeconds(
+                    TimeUnit.MILLISECONDS.toMinutes(timeInMilliseconds)
+                )
+            )
+        )
+    }
+}
